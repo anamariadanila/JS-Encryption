9ecaf9d67ecf2c1752380c4f284e063bac8a3a11
Dănilă Ana-Maria <anamaria1616d@gmail.com>
2022-07-25 23:40:54 +0300

    style & dom manipulation added

--- app.js
+++ app.js
@@ -83,7 +83,9 @@ const encryptMessage = function (message, key, type) {
       newMessage += message.toUpperCase()[i];
     }
   }
+
   console.log(newMessage);
+  return newMessage;
 };
 
 encryptMessage("abcde", "bbbbb", "encrypt");
@@ -106,7 +108,42 @@ const decryptMessage = function (message, key, type) {
     }
   }
   console.log(newMessage);
+  return newMessage;
 };
 
 decryptMessage("abdde", "aab", "decrypt");
 decryptMessage("bcdef", "bbbbb", "decrypt");
+
+const encBtn = document.getElementById("btn-encrypt");
+const decBtn = document.getElementById("btn-decrypt");
+const copyBtn = document.getElementById("btn-copy");
+
+encBtn.addEventListener("click", () => {
+  const type = "encrypt";
+  const message = document.getElementById("initial-message").value;
+  const key = document.getElementById("secret-code").value;
+  document.getElementById("enc-message").innerHTML = encryptMessage(
+    message,
+    key,
+    type
+  );
+  document.getElementById("enc-dec").innerHTML = "Your encrypted message: ";
+});
+
+decBtn.addEventListener("click", () => {
+  const type = "decrypt";
+  const message = document.getElementById("initial-message").value;
+  const key = document.getElementById("secret-code").value;
+  document.getElementById("enc-message").innerHTML = decryptMessage(
+    message,
+    key,
+    type
+  );
+  document.getElementById("enc-dec").innerHTML = "Your decrypted message: ";
+});
+
+copyBtn.addEventListener("click", () => {
+  const newEncMessage = document.getElementById("enc-message").value;
+  navigator.clipboard.writeText(newEncMessage);
+  window.location.reload("Refresh");
+});
